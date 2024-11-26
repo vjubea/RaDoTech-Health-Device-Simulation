898c1bdfe2cf0dcb40087676b56152f9adc3b7d9
student <xrichardisfast@gmail.com>
2024-11-26 14:54:51 -0500

    Fleshed out scanner

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -8,26 +8,55 @@ Scanner::Scanner(Profile* profile) : profile(profile)
 }
 
 
-void registerReading(char side, char limb, int point, float reading){
+void Scanner::registerReading(char side, char limb, int point, float reading){
+    float* arr;
+    if(side == 'l'){
+        if(limb == 'h') {arr = lHandRead;}
+        else {arr = lFootRead;}
+    }
+    else {
+        if(limb == 'h'){arr = rHandRead;}
+        else {arr = rFootRead;}
+    }
+    arr[point] = reading;
 
 }
 
 
-void registerBlood(char side, int dyst, int syst){
+void Scanner::registerBlood(char side, int dyst, int syst){
+    int* arr;
+    if(side == 'l'){arr = lHandBlood;}
+    else {arr = rHandBlood;}
+    arr[0] = dyst;
+    arr[1] = syst;
 
 }
 
 
-void registerWeight(int w){
-
+void Scanner::registerWeight(int w){
+    if(w == -1){
+        w = profile->getWeight();
+    }
+    weight = w;
 }
 
+void Scanner::registerHeartRate(int h){heartRate = h;}
 
-int finishScan(){
+void Scanner::registerBodyTemp(float t){bodyTemp = t;}
+
+void Scanner::registerDate(int m, int d, int y){date[0] = m; date[1] = d; date[2] = y;}
+
+void Scanner::registerTime(int h, int m){time[0] = h; time[1] = m;}
+
+void Scanner::registerSleepTime(int h, int m){sleepTime[0] = h; sleepTime[1] = m;}
+
+
+
+int Scanner::finishScan(){
     // this functionshould place everything into the database as a scan. Values not held that are known of the profile should be retrieved as well
     // data in this object should then be cleaned up.
 
 
-    //return ID of the scan that was just created in the db.
+    //return ID of the scan that was just created in the db. This can be used in the GUI to immediately retrieve the scan and display it.
     return 0;
 }
--- src/scanner.h
+++ src/scanner.h
@@ -1,6 +1,8 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+#include<string>
+
 //imports in .cpp to avoid circular imports in headers
 class Profile;
 
@@ -14,14 +16,20 @@ There should only ever be one scanner object at a time for this application's de
 private:
     Profile* profile;
 
+    int date[3]; // mm/dd/yyy
+    int time[2]; // hh:mm
+
     float rHandRead[6];
     float lHandRead[6];
     float rFootRead[6];
     float lFootRead[6];
     int rHandBlood[2];
     int lHandBlood[2];
-    int weight;
-    //fill in other fields to register
+    int weight; //in grams
+    int heartRate;
+    float bodyTemp; //in Celcius
+    int sleepTime[2]; // hh:mm
+    std::string text;
 
 
 public:
@@ -30,6 +38,11 @@ public:
     void registerReading(char side, char limb, int point, float reading);
     void registerBlood(char side, int dyst, int syst);
     void registerWeight(int w);
+    void registerHeartRate(int h);
+    void registerBodyTemp(float t);
+    void registerDate(int m, int d, int y);
+    void registerTime(int h, int m);
+    void registerSleepTime(int h, int m);
 
     int finishScan();
 };
