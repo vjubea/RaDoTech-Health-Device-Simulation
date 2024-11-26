612e81dd2d880387678da26f690c664cb578f449
Nigel <nigelklose02@gmail.com>
2024-11-25 20:09:34 -0500

    Create dbmanager.h

---
+++ src/dbmanager.h
@@ -0,0 +1,24 @@
+#ifndef DBMANAGER_H
+#define DBMANAGER_H
+
+#include <QString>
+#include <QSqlDatabase>
+#include <QDateTime>
+#include <QDebug>
+#include <QSqlQuery>
+#include <QList>
+#include <QApplication>
+
+class DBManager {
+
+public:
+    
+    DBManager();
+    
+
+
+private:
+    
+};
+
+#endif // DBMANAGER_H
