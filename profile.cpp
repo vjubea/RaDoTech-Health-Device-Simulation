0035e0e9edda33c517b7d05de65bda816891d5e4
MidnightRaven <132496636+DarkPrimus@users.noreply.github.com>
2024-11-26 22:21:35 -0500

    Add files via upload

---
+++ profile.cpp
@@ -0,0 +1,30 @@
+#include "profile.h"
+
+Profile::Profile() : id(0), weight(0), height(0) {}
+
+Profile::Profile(
+    int id, const QString& fname, const QString& lname, float weight,
+    float height, const QString& bday)
+    :
+    id(id), fname(fname), lname(lname), weight(weight),
+    height(height), bday(bday)
+{}
+
+int Profile::getId() const { return id; }
+void Profile::setId(int id) { this->id = id; }
+
+QString Profile::getFname() const { return fname };
+void Profile::setFname(const QString& fname) { this->fname = fname; }
+
+QString Profile::getLname() const { return lname; }
+void Profile::setLname(const QString& lname) { this->lname = lname; }
+
+float Profile::getWeight() const { return weight; }
+void Profile::setWeight(float weight) { this->weight = weight; }
+
+float Profile::getHeight() const { return height; }
+void Profile::setHeight(float height) { this->height = height; }
+
+QString Profile::getBday() const { return bday; }
+
+void Profile::setBday(const QString& bday) { this->bday = bday; }
---
+++ profile.h
@@ -0,0 +1,40 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+#include <QString>
+
+class Profile {
+public:
+    // Constructors
+    Profile();
+    Profile(int id, const QString& fname, const QString& lname, float weight, float height, const QString& bday, const QString& country);
+
+    // Getters and Setters
+    int getId() const;
+    void setId(int id);
+    
+    QString getFname() const;
+    void setFname(const QString& fname);
+
+    QString getLname() const;
+    void setLname(const QString& lname);
+
+    float getWeight() const;
+    void setWeight(float weight);
+
+    float getHeight() const;
+    void setHeight(float height);
+
+    QString getBday() const;
+    void setBday(const QString& bday);
+
+private:
+    int id;  // Profile ID
+    QString fname;  // First name
+    QString lname;  // Last name
+    float weight;  // Weight in kg
+    float height;  // Height in cm
+    QString bday;  // Birthdate (mm-dd-yyyy)
+};
+
+#endif // PROFILE_H
